02ca2e0f91653b0a5e9e9c1c0e2e1796250bc8cc
kangshy <kangshy@gmail.com>
2022-05-14 10:27:50 +0900

    Axio 이용하기
    Add axios usage project with Kakao API

--- src/components/AxiosView.vue
+++ src/components/AxiosView.vue
@@ -1,80 +1,82 @@
 <template>
     <div>
-    <select v-model="mode" @change="log">
-        <option value="doc">웹문서</option>
-        <option value="mov">동영상</option>
-        <option value="img">이미지</option>
-    </select>
-      <input type="text" v-model="search" @keyup.enter="callData">
-      <button @click="callData">검색</button>
+        <select v-model="mode">
+            <option value="doc">웹문서</option>
+            <option value="mov">동영상</option>
+            <option value="img">이미지</option>
+        </select>
+        <input type="text" v-model="search" @keyup.enter="callData">
+        <button @click="callData">검색</button>
 
-      <table>
-        <div v-if="mode='doc'">
-            <tr>
-                <th>제목</th>
-                <th>내용</th>
-            </tr>
-            <tr v-for="(item, index) in list" :key="index">
-                <td>
-                    <a href="item.url" target="_blank">
-                    <span v-html="iteml.title"></span>
-                    </a>
-                </td>
-                <td>
-                    <span v-html="item.contents"></span>
-                </td>
-            </tr>
-        </div>
-        <div v-else-if="mode='mov'">
-            <tr>
-                <th>제목</th>
-                <th>내용</th>
-            </tr>
-            <tr v-for="(item, index) in list" :key="index">
-                <td>
-                    <a href="item.url" target="_blank">
+        <table v-if='list.length!=0'>
+            <div v-if="mode='doc'">
+                <tr>
+                    <td colspan=2 align="right">{{this.meta.pageable_count}} / {{this.meta.total_count }}</td>
+                </tr>
+                <tr>
+                    <th>제목</th>
+                    <th>내용</th>
+                </tr>
+                <tr v-for="(item, index) in list" :key="index">
+                    <td>
+                        <a :href="item.url" target="_blank">
                         <span v-html="iteml.title"></span>
-                    </a>
-                </td>
-                <td>
-                    <span v-html="item.thumbnail"></span>
-                </td>
-            </tr>
-        </div>
-        <div v-else-if="mode='img'">
-            <tr>
-                <th>제목</th>
-                <th>내용</th>
-            </tr>
-            <tr v-for="(item, index) in list" :key="index">
-                <td>   
-                    <a href="item.url" target="_blank">
-                        <span v-html="iteml.title"></span>
-                    </a>
-                </td>
-                <td>
-                    <span v-html="item.contents"></span>
-                </td>
-            </tr>
-        </div>
-      </table>
-  </div>
+                        </a>
+                    </td>
+                    <td>
+                        <span v-html="item.contents"></span>
+                    </td>
+                </tr>
+            </div>
+            <div v-else-if="mode='mov'">
+                <tr>
+                    <th>제목</th>
+                    <th>내용</th>
+                </tr>
+                <tr v-for="(item, index) in list" :key="index">
+                    <td>
+                        <a :href="item.url" target="_blank">
+                            <span v-html="item.title"></span>
+                        </a>
+                    </td>
+                    <td>
+                        <a :href="item.url"><img :src="item.thumbnail" _targte="blank"></a>
+                    </td>
+                </tr>
+            </div>
+            <div v-else-if="mode='img'">
+                <tr>
+                    <th>제목</th>
+                    <th>내용</th>
+                </tr>
+                <tr v-for="(item, index) in list" :key="index">
+                    <td>
+                        <span><a :href="item.doc_url">{{item.display_sitename}}</a></span>
+                    </td>
+                    <td>
+                        <span><img :src="item.thumbnail_url"></span>
+                    </td>
+                </tr>
+            </div>
+        </table>
+        <div v-else>검색 결과를 찾을 수 없습니다</div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data : () => ({
         search : '',
         list : [],
+        meta : {},
         mode : 'doc',
     }),
 
     methods : {
-        log() {
-            console.log(this.mode);
-        },
         callData() {
-            console.log(list.length);
+            //console.log(this.list.length);
             switch(this.mode){
                 case 'doc' :
                     console.log(this.mode);
@@ -95,7 +97,9 @@ export default {
                 }
             }).then(response=>{
                 this.list=response.data.documents;
+                this.meta=response.data.meta;
             }).catch(error=>{
+                console.log("[Error] " +error);
                 console.error(error);
             });
         },
